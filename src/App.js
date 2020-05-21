dee64a3fb62435facfc2f42fae6569f0fa881cc1
Brian Bower <brian@brianbower.com>
2020-05-21 09:47:20 -0600

    Add custom logo and content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './babslabs-monkey-logo-portfolio.png';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Portfolio Site Coming Soon
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/BabsLabs/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub
         </a>
       </header>
     </div>
